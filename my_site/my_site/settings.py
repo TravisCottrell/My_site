4df2f1109a20e04d6c201f30ecf6877f09b1277e
TravisCottrell <tcottre4@kent.edu>
2021-07-24 17:44:38 -0400

    setup for deploy

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x-o=uq$+-6z_r10g4%-qdopatq59_9^ik^lu#ff^qdoy(%)#vy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["traviscottrell.herokuapp.com","127.0.0.1"]
 
 
 # Application definition

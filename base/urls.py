55172a87c0762356078fcadc3947e9d7cf285233
TravisCottrell <tcottre4@kent.edu>
2022-01-20 16:25:05 -0500

    added AI_car/ added other projects section'

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('imageSearch', views.imageSearch, name='imageSearch'),
     path('dataTracker', views.dataTracker, name='dataTracker'),
+    path('AI_Car', views.AI_Car, name='AI_Car'),
+    path('HMM', views.HMM, name='HMM'),
+    path('Kmeans', views.Kmeans, name='Kmeans'),
+    path('LinReg', views.LinReg, name='LinReg'),
 ]
